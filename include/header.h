c747c6443970c2ee7ed8dfc6fc375be4d8eef442
Fardin Rahman <71291566+Fardin-E@users.noreply.github.com>
2024-12-22 02:23:33 -0500

    Starting from scratch

--- include/header.h
+++ include/header.h
@@ -4,23 +4,3 @@
 #include "raylib.h"
 #include "raymath.h"
 
-#define MAX_TEXTURES 2
-
-// Declare the textures array
-extern Texture2D textures[MAX_TEXTURES];
-
-constexpr float TILE_WIDTH = 192;
-constexpr float TILE_HEIGHT = 192;
-
-struct sTile {
-    float x;
-    float y;
-
-    sTile(float x_ = 0, float y_ = 0)
-        : x(x_)
-        , y(y_)
-    {}
-};
-
-// Declare the DrawWarrior function
-void DrawTile(float pos_x, float pos_y, float texture_index_x, float texture_index_y, int texture);
